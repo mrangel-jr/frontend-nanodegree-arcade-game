0040d66f0e99c07ef91b431bd063a8474dd743e0
mrangel-jr <mrangel@gmail.com>
2018-11-28 00:35:01 -0200

    Adding new functions and create Character class

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-frontend-nanodegree-arcade-game
+Frontend Nanodegree Arcade Game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## Getting Started
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+It's simple ! You need clone the project and open index.html in you browser !
+
+```bash
+git clone https://github.com/mrangel-jr/frontend-nanodegree-arcade-game.git
+cd frontend-nanodegree-arcade-game
+open index.html
+```
+
+## How play the game ??
+
+Did you see the game ??
+
+![Arcade Game Playing with John](images/screen.png)
+
+This is John !! Your goal is avoid the collision with bugs and get to the water. When you complete it, you get one life !! However, if any bug catch you, you lose one life.
 
--- js/app.js
+++ js/app.js
@@ -1,104 +1,144 @@
+'use strict;'
+
+
+/* Create a superclass Character that take commom params from Player and Enemy */
+class Character {
+
+	/*This is a constructor passing the commom params*/
+	constructor(x, y, firstX, firstY, lastX, lastY, sprite) {
+		this.firstX = firstX;
+		this.firstY = firstY;
+		this.lastX = lastX;
+		this.lastY = lastY;
+		this.x = x;
+		this.y = y;
+		this.sprite = sprite;
+	}
+
+}
+
 // Enemies our player must avoid
-var Enemy = function (type, pos) {
-	// Variables applied to each of our instances go here,
-	// we've provided one for you to get started
-	this.positions = {
-		first: 63,
-		middle: 146,
-		last: 229
+/* Enemy class extends Character superclass !! */
+class Enemy extends Character {
+
+	constructor(type, pos) {
+		/*Setting values to commom params in Superclass */
+		super(0, 0, 0, 63, 404, 63, 'images/enemy-bug.png');
+		/* Types of Velocity that move each bugs */
+		this.steps = {
+			turtle: 50,
+			rabbit: 100,
+			hurricane: 200
+		};
+		/* Positions of bug in panel */
+		this.positions = {
+			first: 63,
+			middle: 146,
+			last: 229
+		};
+		/*This is an override of position! I can't declare a property with "this" before super method.
+		And i need this.positions in another method, like update. So i pass a default value in super 
+		method and i make a override here !! */
+		this.y = this.positions[pos];
+		/*Here i set the velocity for each bug. This velocity was passed in constructor*/
+		this.pass = this.steps[type];
+	}
+
+	update(dt) {
+		// You should multiply any movement by the dt parameter
+		// which will ensure the game runs at the same speed for
+		// all computers.
+		/*Using the velocity for each bug, here update the movement the bug*/
+		this.x += this.pass * dt;
+		if (this.x > 500) {
+			this.x = 0;
+			/*Here is magic ! I created bugs and randomly it took a different position for each one.*/
+			this.y = this.positions[Object.keys(this.positions)[Math.floor(Math.random() * Object.keys(this.positions).length)]];
+		}
+		return;
 	};
-	this.firstX = 0;
-	this.firstY = 63;
-	this.lastX = 404;
-	this.lastY = 63;
-	this.x = 0;
-	this.y = this.positions[pos];
-	this.steps = {
-		turtle: 50,
-		rabbit: 100,
-		hurricane: 200
-	}
-	this.pass = this.steps[type];
-	// The image/sprite for our enemies, this uses
-	// a helper we've provided to easily load images
-	this.sprite = 'images/enemy-bug.png';
-};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
-	// You should multiply any movement by the dt parameter
-	// which will ensure the game runs at the same speed for
-	// all computers.
-	this.x += this.pass * dt;
-	if (this.x > 500) {
-		this.x = 0;
-		this.y = this.positions[Object.keys(this.positions)[Math.floor(Math.random() * Object.keys(this.positions).length)]];
-	}
-	return;
-};
+	checkCollision(player) {
+		// You should multiply any movement by the dt parameter
+		// which will ensure the game runs at the same speed for
+		// all computers.
 
-//Check collision between enemies and player.
-//Return True if has collision. Otherwise, return false
+		/*Magic Value !!! Using this value, the condition below take more realistic when 
+		the bug hit the player */
+		const dx = 70;
+		if (((player.getY() - this.y) === 5) && (parseInt(this.x) >= player.getX() - dx && (parseInt(this.x) <= player.getX() + dx))) {
+			return true;
+		}
+		return false;
+	};
 
-Enemy.prototype.checkCollision = function (player) {
-	// You should multiply any movement by the dt parameter
-	// which will ensure the game runs at the same speed for
-	// all computers.
-	//63,146,229
-	dx = 2;
-	if (((player.getY() - this.y) === 5) && (parseInt(this.x) >= player.getX() - dx && (parseInt(this.x) <= player.getX() + dx))) {
-		return true;
-	}
-	return false;
-};
-
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
+	render() {
+		/*Just render the bug on screen*/
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	};
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-class Player {
+
+class Player extends Character {
 
 	constructor(name) {
+		/*Same process on Enemy constructor function*/
+		super(202, 400, 0, 0, 404, 400, 'images/char-boy.png');
+		/*Set name of player*/
 		this.name = name;
-		this.sprite = 'images/char-boy.png';
-		this.firstX = 0;
-		this.firstY = 0;
-		this.lastX = 404;
-		this.lastY = 400;
-		this.x = 202;
-		this.y = 400;
-		this.bonus = 0;
+		/*How many lifes it begin*/
+		this.bonus = 3;
+		/*Start show lifes on screen*/
+		this.updateBonus();
 	}
 
 	update() {
+		/*Update position on screen of Player. If hit on water, he win a life, 
+		i update it on screen and reset the player*/
+		//hit on water
 		if (this.y < 0) {
+			//he win a life
 			this.bonus++;
-			console.log(`${this.name} has ${this.bonus}`);
+			//i update it on screen
+			this.updateBonus();
+			//reset the player
 			this.y = this.lastY;
 		}
 	}
 
+	/*I update the number of lifes on screeen*/
+	updateBonus() {
+		document.querySelector('#lifes').innerText = this.bonus;
+		document.querySelector('#message').innerText = `${this.name} has ${this.bonus} lifes`;
+		setTimeout(() => {
+			document.querySelector('#message').innerText = ' ';
+		}, 2000);
+	}
+
+	/*If had any collision, the player lost a life, i update it on screen and reset the player*/
 	lostLife() {
+		/*lost a life*/
 		this.bonus--;
-		console.log(`${this.name} has ${this.bonus}`);
+		/*Update new lifes on screen*/
+		this.updateBonus();
+		/*reset the player*/
 		this.y = this.lastY;
 	}
 
+	/*Auxiliar method to return player's X position to checkCollision in Enemy Class*/
 	getX() {
 		return this.x;
 	}
 
+	/*Auxiliar method to return player's Y position to checkCollision in Enemy Class*/
 	getY() {
 		return this.y;
 	}
 
+	/*Just move the player on screen to up, down, left, right*/
 	handleInput(move) {
 		switch (move) {
 			case 'left':
@@ -136,10 +176,13 @@ class Player {
 		}
 	}
 
+	/*Render player character*/
 	render() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	};
 }
+
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
